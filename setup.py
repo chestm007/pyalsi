4f312445eacb848bd7ffb97b403059bf413ebd6f
Max Chesterfield <max@instaclustr.com>
2017-03-11 01:16:32 +1100

    bump version for release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pyalsi',
-    version='0.4.1',
+    version='0.4.2',
     packages=find_packages(),
     install_requires=['psutil', 'click', 'py-cpuinfo'],
     license='GPL2',
