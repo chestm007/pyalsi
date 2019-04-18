68909fe6350efd7db3321e88e27872d586435504
Max <chestm007@hotmail.com>
2019-04-19 03:23:32 +1000

    0.5.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pyalsi',
-    version='0.5.2',
+    version='0.5.3',
     packages=find_packages(),
     install_requires=['psutil', 'click', 'py-cpuinfo'],
     license='GPL2',
